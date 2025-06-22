f7ab81ee3251bf28c144159e33132890bab01d4b
mipo256 <mikhailpolivakha@gmail.com>
2025-06-22 21:27:03 +0300

    kai2 2025: refactoring

---
+++ filterRealGitHubProjects.js
@@ -0,0 +1,186 @@
+const octokit = require("./githubClient")
+const fs = require('fs')
+const path = require('path');
+const { delay } = require('./common')
+
+
+const startDate = new Date(process.env.START_DATE ?? '2019-01-01')
+const lastDate = new Date(process.env.LAST_DATE ?? '2023-05-01')
+
+/**
+ * Read list of submitted GitHub projects, check it on Github:
+ * - not private
+ * - not template
+ * - at least five years old
+ * - not arhcived
+ * Then add to the list
+ * @async
+ */
+async function filterRealGitHubProjects() {
+  const repos = distillRepos()
+
+  for (let i = 0; i < repos.length; i++) {
+    if (!repos[i].startsWith('https://github.com')) {
+      continue
+    }
+    const repo = repos[i].split('github.com/')[1]
+    if (repo.indexOf('/') === -1) {
+      continue
+    }
+    try {
+      const response = await octokit.request('GET https://api.github.com/repos/' + repo)
+      if (response.status === 200) {
+        let data = response.data
+        if (passesAllQualityChecks(data, repo)) {
+          fs.appendFileSync(path.resolve('files/repos.txt'), `* [${repo}](${repos[i]})\n`)
+          console.log(`Repo '${data.name}' passed validation'`)
+        } else {
+          console.log(`Repo '${data.name}' got filtered out'`)
+        }
+        await delay(500)
+      } else {
+        console.log('Something went wrong: ', response)
+      }
+    } catch (e) {
+      console.log('Request fail: ', e.message)
+    }
+  }
+}
+
+function passesAllQualityChecks(data, repo) {
+  return isRealGitHubRepo(data) && 
+         hasRequierdNumberOfReleases(repo) && 
+         hasLicense(data) && 
+         hasSufficientlyLongReadme(repo) && 
+         hasSufficentAmountOfIssues(data) &&
+         hasSufficentAmountOfCommits(data)
+}
+
+/**
+ * @param {*} data 
+ * @returns true if the project has a defined license, false otherwise
+ */
+function hasLicense(data) {
+  return !data.license || !data.license?.key
+}
+
+async function hasSufficentAmountOfIssues(data, expectedIssuesCount = process.env.ISSUES_COUNT) {
+
+  req = await octokit.request(`GET ${data.issues_url}`, {
+    state: 'all',
+    per_page: expectedIssuesCount,
+    page: 1
+  })
+
+  await delay(300)
+  
+  if (req.status === 200) {
+    return expectedIssuesCount >= req.data.length
+  } else {
+    throw new Error(`Unable to fetch the issues info for the project ${data.issues_url}`)
+  }
+}
+
+async function hasSufficentAmountOfCommits(data, expectedCommitsCount = process.env.COMMITS_COUNT) {
+
+  req = await octokit.request(`GET ${data.commits_url}`, {
+    per_page: expectedCommitsCount,
+    page: 1
+  })
+
+  await delay(300)
+
+  if (req.status === 200) {
+    return req.data.length >= expectedCommitsCount
+  } else {
+    throw new Error(`Unable to discover the amount of commits in the project: ${data.commits_url}`)
+  }
+}
+
+/**
+ * Chech if the project's README has the expected amount of lines in it
+ * 
+ * @param {string} projectUrls - the url of a given project
+ * @param {number} readmeLines - the expected amount of lines in README
+ */
+async function hasSufficientlyLongReadme(projectUrls, readmeLines = process.env.README_LINES) {
+  req = await octokit.request(`GET /repos/${projectUrls}/readme`)
+  
+  if (req.status === 200) {
+    await delay(300)
+  
+    console.log(req)
+  
+    req = await axios.get(req.data.download_url)
+    const lines = req.data.split('\n')
+  
+    return lines.length >= readmeLines
+  } else {
+    throw new Error(`Unable to discover the README of project: ${projectUrls}`)
+  }
+} 
+
+/**
+ * @param {string} projectUrls - the url of the project
+ * @param {number} releasesNum - required number of releases  
+ * @returns 
+ */
+async function hasRequierdNumberOfReleases(projectUrls, releasesNum = process.env.MIN_RELEASES) {
+  console.log(`Cecking the releases count for project ${projectUrls}, expected: ${releasesNum}`)
+
+  req = await octokit.request(`GET /repos/${url}/releases`, {
+    per_page: releasesNum,
+    page: 1
+  })
+  
+  if (req.status === 200) {
+    return req.data.length >= releasesNum
+  } else {
+    throw new Error(`Unable to determine the amount of releases for a project: ${projectUrls}`)  
+  } 
+}
+
+/**
+ * Perform basic checks for the project to make sure the project is open source and maintained
+ * @param {*} data 
+ */
+function isRealGitHubRepo(data) {
+    return data.private === false
+          && new Date(data.created_at) >= startDate
+          && new Date(data.created_at) <= lastDate
+          && data.archived === false
+          && data.disabled === false
+          && data.is_template === false
+}
+
+/**
+ * @returns {Array<string>} the alphabetically sorted array of repositories, that passed initial filtering
+ */
+function distillRepos() {
+    return [...new Set(
+    fs.readFileSync(path.resolve(__dirname, 'files/projects.txt'))
+      .toString()
+      .split('\n')
+      .map((repo) => cleanUpUrl(repo))
+      .filter(rp => {
+        return !rp.includes('/vocably/') && !rp.slice(rp.indexOf('github.com') + 10).includes('github.com')
+      })
+      .sort((a, b) => a.localeCompare(b))
+  )]
+}
+
+/**
+ * @param {string} repo - the name of the repo
+ * @returns @returns {string} the cleaned-up repo URL 
+ */
+function cleanUpUrl(repo) {
+    let rp = repo
+        .replace('GitHub.com', 'github.com')
+        .replace('http://', 'https://')
+    if (rp.startsWith('github.com')) {
+        rp = rp.replace('github.com', 'https://github.com')
+    }
+    return rp
+}
+
+module.exports = { checkProjects: filterRealGitHubProjects }
--- index.js
+++ index.js
@@ -1,232 +1,11 @@
+const {octokit} = require('./githubClient')
+const { filterRealGitHubProjects } = require('./filterRealGitHubProjects')
+const { delay, isSourceCodeFile } = require('./common')
+
 const axios = require('axios');
-const {Octokit, App} = require('octokit')
 const fs = require('fs');
 const path = require('path');
 const {execSync} = require('child_process');
-
-const octokit = new Octokit({
-  auth: '' // your GitHub token here
-});
-
-function addDays(date, days) {
-  let result = new Date(date);
-  result.setDate(result.getDate() + days);
-  return result;
-}
-
-// Search github repositories
-async function searchRepositories(ids, first, second) {
-  const filter = new Set(ids)
-  const owners = [];
-  let total = 1
-  let page = 1
-  let data
-  while (page <= total) {
-    try {
-      const response = await octokit.request('GET https://api.github.com/search/repositories', {
-        q: [
-          'stars:100..1000',
-          `created:${first.toISOString()}..${second.toISOString()}`,
-          'is:public',
-          'template:false',
-          'archived:false',
-          'size:>1000',
-          'mirror:false',
-          'language:C++',
-          'language:C#',
-          'language:Java',
-          'language:JavaScript',
-          'language:TypeScript',
-          'language:Python',
-          'language:PHP',
-          'language:Ruby',
-          'language:Go',
-          'language:Rust',
-          'language:Kotlin'
-        ].join(' '),
-        per_page: 100,
-        // per_page: 1,
-        page: page
-      })
-      if (response.status === 200) {
-        data = response.data
-        console.log(data.total_count)
-        // console.log(data.items[0].owner)
-        for (let i = 0; i < data.items.length; i++) {
-          if (!filter.has(data.items[i].owner.login)) {
-            owners.push(data.items[i].owner.login)
-            filter.add(data.items[i].owner.login)
-          }
-        }
-        total = Math.ceil(response.data.total_count / 100)
-        page++
-      } else {
-        console.log('Something went wrong: ', response)
-      }
-      setTimeout(() => {
-      }, 300)
-    } catch (e) {
-      console.log('Request fail: ', e.message)
-    }
-  }
-  return owners
-}
-
-async function getRepositories() {
-  const pth = path.resolve(__dirname, 'files/hundred.txt')
-  const content = fs.readFileSync(pth).toString()
-  const split = content.split('\n--')
-  split.pop()
-  let ids = []
-  let first, second
-  if (content.length === 0) {
-    first = new Date('2019-01-01')
-  } else {
-    for (let i = 0; i < split.length; i++) {
-      const lines = split[i].split('\n')
-      first = addDays(new Date(lines.shift()), 15)
-      for (let j = 0; j < lines.length; j++) {
-        ids.push(lines[j])
-      }
-    }
-  }
-  const finish = new Date('2023-05-01')
-  second = addDays(first, 15)
-
-  while (second <= finish) {
-    console.log(first.toISOString() + '--' + second.toISOString())
-
-    let owners = await req(ids, first, second)
-    ids = new Set([...ids, ...owners])
-    console.log('written', owners.length)
-    fs.appendFileSync(pth, `${first.toISOString()}\n${owners.join('\n')}\n--`)
-
-    first = second
-    second = addDays(second, 15)
-  }
-}
-
-async function getEmails() {
-  const pth = path.resolve(__dirname, 'files/pre.txt')
-  const epth = path.resolve(__dirname, 'files/ehundred.txt')
-  console.log(pth)
-  const content = fs.readFileSync(pth).toString()
-  const split = content.split('\n--')
-  split.shift()
-  split.pop()
-  let logins = []
-  if (content.length !== 0) {
-    for (let i = 0; i < split.length; i++) {
-      const lines = split[i].split('\n')
-      lines.shift()
-      logins = [...logins, ...lines]
-    }
-  }
-  console.log('unique logins', logins.length)
-  let data
-
-  for (let i = 0; i < logins.length; ++i) {
-    try {
-      const response = await octokit.request('GET https://api.github.com/users/' + logins[i])
-      if (response.status === 200) {
-        data = response.data
-        if (data.email && data.type !== 'Organization' && data.name) {
-          console.log('written', logins[i], data.name, data.email)
-          fs.appendFileSync(epth, `${logins[i]} || ${data.name} || ${data.email}\n`)
-        }
-        console.log('left', logins.length - i)
-        await delay(770)
-      } else {
-        console.log('Something went wrong: ', response)
-      }
-    } catch (e) {
-      console.log('Request fail: ', e.message)
-    }
-  }
-}
-
-function delay(milliseconds) {
-  return new Promise(resolve => {
-    setTimeout(resolve, milliseconds);
-  });
-}
-
-function process() {
-  const pth = path.resolve(__dirname, 'files/ehundred.txt')
-  const lines = fs.readFileSync(pth).toString().split('\n')
-  let res = []
-  for (let i = 0; i < lines.length; i++) {
-    const data = lines[i].split(' || ')
-    res.push(`${data[0]},${data[1]},${data[2]}`)
-  }
-  fs.writeFileSync(
-    path.resolve(__dirname, 'users.csv'),
-    res.join('\n')
-  )
-}
-
-// Read list of submitted GitHub projects, check it on Github:
-// - not private
-// - not template
-// - at least five years old
-// - not archieved
-// Then add to the list
-async function checkProjects() {
-  const repos = [...new Set(
-    fs.readFileSync(path.resolve(__dirname, 'files/projects.txt'))
-      .toString()
-      .split('\n')
-      .map((repo) => {
-        let rp = repo
-          .replace('GitHub.com', 'github.com')
-          .replace('http://', 'https://')
-        if (rp.startsWith('github.com')) {
-          rp = rp.replace('github.com', 'https://github.com')
-        }
-        return rp
-      })
-      .filter(rp => {
-        return !rp.includes('/vocably/') && !rp.slice(rp.indexOf('github.com') + 10).includes('github.com')
-      })
-      .sort((a, b) => a.localeCompare(b))
-  )]
-  const startDate = new Date('2019-01-01')
-  const lastDate = new Date('2023-05-01')
-  for (let i = 0; i < repos.length; i++) {
-    if (!repos[i].startsWith('https://github.com')) {
-      continue
-    }
-    const repo = repos[i].split('github.com/')[1]
-    if (repo.indexOf('/') === -1) {
-      continue
-    }
-    try {
-      const response = await octokit.request('GET https://api.github.com/repos/' + repo)
-      if (response.status === 200) {
-        let data = response.data
-        console.log(data.name, data.created_at)
-        if (data.private === false
-          && new Date(data.created_at) >= startDate
-          && new Date(data.created_at) <= lastDate
-          && data.archived === false
-          && data.disabled === false
-          && data.is_template === false
-        ) {
-          fs.appendFileSync(
-            path.resolve('repos.txt'),
-            `* [${repo}](${repos[i]})\n`
-          )
-          console.log('added')
-        }
-        await delay(500)
-      } else {
-        console.log('Something went wrong: ', response)
-      }
-    } catch (e) {
-      console.log('Request fail: ', e.message)
-    }
-  }
-}
 
 async function filterRepos() {
   const repos = fs.readFileSync(path.resolve(__dirname, 'files/repos.txt'))
@@ -234,7 +13,7 @@ async function filterRepos() {
     .split('\n')
     .map((repo) => repo
       .slice(repo.indexOf('](') + 2, repo.length - 1)
-      .slice(19)
+      .slice('https://github.com/'.length)
   )
   const result = []
   for (let idx in repos) {
@@ -259,6 +38,7 @@ async function filterRepos() {
             continue
           }
         } else {
+          // TODO: that is not fair, it should be re-tried
           continue
         }
 
@@ -270,10 +50,7 @@ async function filterRepos() {
         await delay(300)
 
         // Check readme
-        console.log('here')
         req = await octokit.request(`GET /repos/${url}/readme`)
-        console.log('here')
-
 
         if (req.status === 200) {
           await delay(300)
@@ -287,6 +64,7 @@ async function filterRepos() {
             continue
           }
         } else {
+          // TODO: that is not fair, it should be re-tried
           continue
         }
 
@@ -303,6 +81,7 @@ async function filterRepos() {
             continue
           }
         } else {
+          // TODO: that is not fair, it should be re-tried 
           continue
         }
 
@@ -313,6 +92,7 @@ async function filterRepos() {
           per_page: 60,
           page: 1
         })
+
         if (req.status === 200) {
           console.log('commits', req.data.length)
 
@@ -320,6 +100,7 @@ async function filterRepos() {
             continue
           }
         } else {
+          // TODO: that is not fair, it should be re-tried
           continue
         }
 
@@ -338,6 +119,7 @@ async function filterRepos() {
             continue
           }
         } else {
+          // TODO: that is not fair, it should be re-tried
           continue
         }
 
@@ -361,15 +143,15 @@ async function filterRepos() {
         console.log('added', url)
         result.push(url)
       } else {
-        console.log('Something went wrong: ', response)
+        console.warn('Something went wrong: ', response)
       }
     } catch (e) {
-      console.log('Request fail: ', e.message)
+      console.warn('Request fail: ', e.message)
     }
   }
 
   result.forEach((repo) => {
-    fs.appendFileSync(path.resolve(__dirname, 'files/releases-5.txt'), `${repo}\n`)
+    fs.appendFileSync(path.resolve(__dirname, 'files/releases.txt'), `${repo}\n`)
   })
 }
 
@@ -397,20 +179,7 @@ function cloneAndFilter() {
           } else if (fd.isFile()) {
             stat.files++
             try {
-              if (
-                // fd.name.endsWith('.py')
-                !fd.name.endsWith('.png')
-                && !fd.name.endsWith('.jpg')
-                && !fd.name.endsWith('.svg')
-                && !fd.name.endsWith('.eot')
-                && !fd.name.endsWith('.ttf')
-                && !fd.name.endsWith('.woff')
-                && !fd.name.endsWith('.pdf')
-                && !fd.name.endsWith('.ico')
-                && !fd.name.endsWith('.rst')
-                && !fd.name.endsWith('.gif')
-                && !fd.name.endsWith('.webp')
-              ) {
+              if (isSourceCodeFile(fd.name)) {
                 const len = fs.readFileSync(path.resolve(dir, fd.name)).toString().split('\n').length
                 if (len >= 1000) {
                   stat.files_1k++
@@ -531,12 +300,13 @@ function urlToMarkdown() {
   )
 }
 
-// getEmails()
-// byEmail()
-// process()
-// checkProjects()
-// filterRepos()
-// cloneAndFilter()
-// checkRepos()
-// top3()
-// urlToMarkdown()
+async function execute() {
+  await filterRealGitHubProjects()
+  await filterRepos()
+  await cloneAndFilter()
+  await checkRepos()
+  await top3()
+  await urlToMarkdown()
+}
+
+execute()
